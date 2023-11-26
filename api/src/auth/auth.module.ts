c613a3548ed3a7d9944b2833355a2fca5baeffc0
cahov <christian.herrera.onaindi@gmail.com>
2023-11-26 17:56:43 -0400

    chore: remove config() library

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -5,12 +5,12 @@ import { JwtModule } from '@nestjs/jwt'
 import { JwtStrategy } from './strategy/jwt.strategy'
 import { UsersService } from 'src/users/users.service'
 import { UsersModule } from 'src/users/users.module'
-import { config } from 'dotenv'
-config()
+import { ConfigModule } from '@nestjs/config'
 @Module({
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, UsersService],
   imports: [
+    ConfigModule.forRoot(),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '7d' }
