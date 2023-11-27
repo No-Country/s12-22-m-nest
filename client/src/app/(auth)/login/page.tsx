541f355c292b9603991e4694aee468236b298cdc
Camila Villaverde <camilavillaverde3@gmail.com>
2023-11-27 12:08:42 -0300

    feat: next auth implementation

---
+++ client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { useState, type FunctionComponent } from 'react'
+import { signIn } from 'next-auth/react'
+
+interface FormData {
+  email: string
+  password: string
+}
+
+const Page: FunctionComponent = () => {
+  const [formData, setFormData] = useState<FormData>({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(formData)
+    const responseNextAuth = await signIn('credentials', {
+      email: formData.email,
+      password: formData.password,
+      redirect: false
+    })
+    console.log(responseNextAuth)
+
+    if (responseNextAuth?.error !== null) {
+        const error = JSON.parse(responseNextAuth?.error as string)
+        const { status, message } = error
+        console.log(message)
+    }
+  }
+
+  return (
+    <form className='flex flex-col' onSubmit={handleSubmit}>
+        <input type='text' name='email' value={formData.email} className='border-2 border-rose-500' onChange={handleChange}/>
+        <input type='password' name='password' value={formData.password} className='border-2 border-rose-500' onChange={handleChange}/>
+        <input type='submit' value={'Enviar'} className='bg-[pink]'/>
+    </form>
+  )
+}
+
+export default Page
