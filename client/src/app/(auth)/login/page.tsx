2858c67a0801720c1dbaeaad04f8118e8ce8558e
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-11-27 12:32:36 -0300

    feat: session in server components

--- client/src/app/(auth)/login/page.tsx
+++ client/src/app/(auth)/login/page.tsx
@@ -13,7 +13,7 @@ const Page: FunctionComponent = () => {
     password: ''
   })
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = e.target
     setFormData((prevData) => ({
       ...prevData,
@@ -21,7 +21,7 @@ const Page: FunctionComponent = () => {
     }))
   }
 
-  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault()
     console.log(formData)
     const responseNextAuth = await signIn('credentials', {
@@ -32,17 +32,34 @@ const Page: FunctionComponent = () => {
     console.log(responseNextAuth)
 
     if (responseNextAuth?.error !== null) {
-        const error = JSON.parse(responseNextAuth?.error as string)
-        const { status, message } = error
-        console.log(message)
+      const error = JSON.parse(responseNextAuth?.error as string)
+      const { message } = error
+      console.log(message)
     }
   }
 
   return (
-    <form className='flex flex-col' onSubmit={handleSubmit}>
-        <input type='text' name='email' value={formData.email} className='border-2 border-rose-500' onChange={handleChange}/>
-        <input type='password' name='password' value={formData.password} className='border-2 border-rose-500' onChange={handleChange}/>
-        <input type='submit' value={'Enviar'} className='bg-[pink]'/>
+    <form
+      className='flex flex-col'
+      onSubmit={(e) => {
+        void handleSubmit(e)
+      }}
+    >
+      <input
+        type='text'
+        name='email'
+        value={formData.email}
+        className='border-2 border-rose-500'
+        onChange={handleChange}
+      />
+      <input
+        type='password'
+        name='password'
+        value={formData.password}
+        className='border-2 border-rose-500'
+        onChange={handleChange}
+      />
+      <input type='submit' value={'Enviar'} className='bg-[pink]' />
     </form>
   )
 }
--- client/src/app/api/auth/[...nextauth]/auth.const.ts
+++ client/src/app/api/auth/[...nextauth]/auth.const.ts
@@ -10,7 +10,7 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 //   json: 'true'
 // }
 
-const authOptions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt'
   },
