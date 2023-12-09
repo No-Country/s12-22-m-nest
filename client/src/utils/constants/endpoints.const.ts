36ce365cdc35b168c4bc5b49c14174c615b735fc
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-12-09 01:09:37 -0300

    refactor: basic client refactor

--- client/src/utils/constants/endpoints.const.ts
+++ client/src/utils/constants/endpoints.const.ts
@@ -8,10 +8,5 @@ export const Endpoints = {
   FIND_ORDER: (id: string) => `/api/orders/${id}`,
   NEXT_STEP: (id: string) => `/api/orders/${id}/nextStep`,
   REGISTER: '/api/auth/register',
-  SEND_MESSAGE: (orderId: string) => `/api/chat/${orderId}/send`,
-  DEALER: '/dealer',
-  DEALER_AVAILABILITY: '/dealer/availability',
-  DEALER_WAITING_ORDER: '/dealer/waiting-order',
-  DEALER_ORDER: (id: string) => `/dealer/order/${id}`,
-  DEALER_ORDER_TRACKING: (id: string) => `/dealer/order-tracking/${id}`
+  SEND_MESSAGE: (orderId: string) => `/api/chat/${orderId}/send`
 }
