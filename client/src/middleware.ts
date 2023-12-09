b2c63b0228649ff047b863d8a82240d8d302da0e
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-12-09 16:06:36 -0300

    feat: auth and role middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,24 +1,32 @@
-import { type NextRequest, NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
-export { default } from 'next-auth/middleware'
+import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
 
-export const middleware = async (req: NextRequest): Promise<NextResponse<unknown>> => {
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET
-  })
-  console.log('session middleware', token?.type, req.nextUrl.pathname, req.nextUrl.pathname.startsWith('/dealer'))
-  if (req.nextUrl.pathname.startsWith('/dealer') && token?.type === 'customer') {
-    console.log('redirecting to /')
-    return NextResponse.redirect(req.nextUrl.origin + '/')
+export default withAuth(
+  async (req) => {
+    const token = await getToken({
+      req,
+      secret: process.env.NEXTAUTH_SECRET
+    })
+    console.log('session middleware', token?.type, req.nextUrl.pathname, req.nextUrl.pathname.startsWith('/dealer'))
+    if (req.nextUrl.pathname.startsWith('/dealer') && token?.type === 'customer') {
+      console.log('redirecting to /')
+      return NextResponse.redirect(req.nextUrl.origin + '/')
+    }
+
+    if (req.nextUrl.pathname.startsWith('/order') && token?.type === 'dealer') {
+      return NextResponse.redirect(req.nextUrl.origin + '/dealer')
+    }
+  },
+  {
+    callbacks: {
+      authorized: (params) => {
+        const { token } = params
+        return !!token
+      }
+    }
   }
-
-  if (req.nextUrl.pathname.startsWith('/order') && token?.type === 'dealer') {
-    return NextResponse.redirect(req.nextUrl.origin + '/dealer')
-  }
-
-  return NextResponse.next()
-}
+)
 
 export const config = {
   matcher: [
