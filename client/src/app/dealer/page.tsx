7accaabeb15ad92810924549410353c69645112a
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-12-09 13:55:02 -0300

    feat: client area --in progress

---
+++ client/src/app/dealer/page.tsx
@@ -0,0 +1,7 @@
+import { type FunctionComponent } from 'react'
+
+const Home: FunctionComponent = () => (
+  <main className='flex min-h-screen flex-col items-center justify-between p-24'>HOME - LANDING PAGE / DEALER</main>
+)
+
+export default Home
