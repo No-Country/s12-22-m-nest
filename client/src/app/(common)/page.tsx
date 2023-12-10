310aebd36530f2734567bd8e3ac1d3779e44ff31
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-12-10 14:13:10 -0300

    fix: comment home

--- client/src/app/(common)/page.tsx
+++ client/src/app/(common)/page.tsx
@@ -1,22 +1,21 @@
-import { ProductGrid } from '@/components'
-import { getProducts } from '@/services/products/getProducts.service'
+// import { ProductGrid } from '@/components'
+// import { getProducts } from '@/services/products/getProducts.service'
 import { type FunctionComponent } from 'react'
-import { type Metadata } from 'next'
+// import { type Metadata } from 'next'
 
-export const metadata: Metadata = {
-  title: 'Inicio | LleGo!'
-}
+// export const metadata: Metadata = {
+//   title: 'Inicio | LleGo!'
+// }
 
-const Home: FunctionComponent = async () => {
-  const { data: products } = await getProducts()
-  return (
-    <main className='padding-general-x flex flex-col gap-10 pb-10 pt-[100px] lg:gap-5 '>
-      <section className='flex w-full flex-col gap-5 2xl:container'>
-        <h2 className='text-2xl font-semibold'>Los mas vendidos 🔥</h2>
-        <ProductGrid products={products ?? []} />
-      </section>
-    </main>
-  )
-}
+const Home: FunctionComponent = async () => (
+  // const { data: products } = await getProducts()
+  <></>
+  // <main className='padding-general-x flex flex-col gap-10 pb-10 pt-[100px] lg:gap-5 '>
+  //   <section className='flex w-full flex-col gap-5 2xl:container'>
+  //     <h2 className='text-2xl font-semibold'>Los mas vendidos 🔥</h2>
+  //     <ProductGrid products={products ?? []} />
+  //   </section>
+  // </main>
+)
 
 export default Home
