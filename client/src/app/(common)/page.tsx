eb5284dfa0be69908f163d17fde17028220d93af
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-12-10 14:24:22 -0300

    fix: working on bug in /

--- client/src/app/(common)/page.tsx
+++ client/src/app/(common)/page.tsx
@@ -1,21 +1,20 @@
-// import { ProductGrid } from '@/components'
+import { ProductGrid } from '@/components'
 // import { getProducts } from '@/services/products/getProducts.service'
 import { type FunctionComponent } from 'react'
-// import { type Metadata } from 'next'
+import { type Metadata } from 'next'
 
-// export const metadata: Metadata = {
-//   title: 'Inicio | LleGo!'
-// }
+export const metadata: Metadata = {
+  title: 'Inicio | LleGo!'
+}
 
 const Home: FunctionComponent = async () => (
   // const { data: products } = await getProducts()
-  <></>
-  // <main className='padding-general-x flex flex-col gap-10 pb-10 pt-[100px] lg:gap-5 '>
-  //   <section className='flex w-full flex-col gap-5 2xl:container'>
-  //     <h2 className='text-2xl font-semibold'>Los mas vendidos 🔥</h2>
-  //     <ProductGrid products={products ?? []} />
-  //   </section>
-  // </main>
+  <main className='padding-general-x flex flex-col gap-10 pb-10 pt-[100px] lg:gap-5 '>
+    <section className='flex w-full flex-col gap-5 2xl:container'>
+      <h2 className='text-2xl font-semibold'>Los mas vendidos 🔥</h2>
+      <ProductGrid products={[]} />
+    </section>
+  </main>
 )
 
 export default Home
